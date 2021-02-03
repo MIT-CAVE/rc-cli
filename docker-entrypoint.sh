5617bc71a66b3e4ca691e16db8a83d026fbcda0e
Connor Makowski <connorman528@gmail.com>
2021-02-03 17:38:46 -0500

    Add some todos in the entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,14 @@ echo "Loading the Docker image..."
 docker load --quiet --input "/mnt/${IMAGE_FILE}"
 
 echo "Running a Docker container..."
+# TODO: Allow for two docker different run items instead of run.sh -> setup.sh and eval.sh
+# TODO: Time both container runs (setup.sh and eval.sh)
 docker run --name=app \
   --mount type=bind,source=${INPUTS_DIR},target=/home/app/data/inputs,readonly \
   --mount type=bind,source=${OUTPUTS_DIR},target=/home/app/data/outputs \
   ${IMAGE_FILE:0:-7} # Remove the '.tar.gz' extension
+
+# TODO: Run MLL Evaluation Script on `rc-out.json` along with the timings for `setup.sh` and `eval.sh`
 
 echo "Done."
 
