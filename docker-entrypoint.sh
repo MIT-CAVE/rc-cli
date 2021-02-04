389b31ff7f6ed676f4f236fd771ab961bf75f55f
Luis Vasquez <luisvasq@mit.edu>
2021-02-04 20:05:05 +0100

    Update rc-cli & entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -eu
 
+is_docker_running() {
+  while ! docker ps; do; sleep 1; done
+}
+
 echo -n "Starting the Docker daemon... "
 /usr/local/bin/dockerd-entrypoint.sh dockerd >& /dev/null & # TODO(luisvasq): supress the stdout
-while ! docker ps >/dev/null; do sleep 1; done # TODO(luisvasq): use a wait command.
-echo "done"
 
+is_docker_running() 2>/dev/null
+
+echo "done"
 echo -n "Loading the Docker image... "
 docker load --quiet --input "/mnt/${IMAGE_FILE}"
 echo "done"
@@ -16,7 +21,7 @@ echo -n "Running the solution container... "
 docker run --name=app \
   --mount type=bind,source=${INPUTS_DIR},target=/home/app/data/inputs,readonly \
   --mount type=bind,source=${OUTPUTS_DIR},target=/home/app/data/outputs \
-  ${IMAGE_FILE:0:-7} # Remove the '.tar.gz' extension
+  "${IMAGE_FILE:0:-7}:rc-cli" # Remove the '.tar.gz' extension
 echo "done"
 
 # TODO: Run MLL Evaluation Script on `rc-out.json` along with the timings for `setup.sh` and `eval.sh`
