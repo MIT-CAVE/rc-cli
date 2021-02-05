b00ffa605262d190babf25933d94ab9b3ae589e5
Connor Makowski <connorman528@gmail.com>
2021-02-05 08:18:30 -0500

    Update Documentation and minor other fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,27 +7,24 @@ is_docker_running() {
   done
 }
 
-echo -n "Starting the Docker daemon... "
+printf "Starting the Docker daemon... "
 /usr/local/bin/dockerd-entrypoint.sh dockerd >& /dev/null &
 
 is_docker_running >& /dev/null
 
-echo "done"
-echo -n "Loading the Docker image... "
+printf "done\n"
+printf "Loading the Docker image... "
 docker load --quiet --input "/mnt/${IMAGE_FILE}" >& /dev/null
-echo "done"
+printf "done\n"
 
-echo "Running the solution container... "
+printf "\n============================\n"
+printf "Running the Solution Image [$IMAGE_FILE]\n\n"
 # TODO: Allow for two docker different run items instead of run.sh -> setup.sh and eval.sh
 # TODO: Time both container runs (setup.sh and eval.sh)
 docker run --name=app \
   --mount type=bind,source=${INPUTS_DIR},target=/home/app/data/inputs,readonly \
   --mount type=bind,source=${OUTPUTS_DIR},target=/home/app/data/outputs \
   "${IMAGE_FILE:0:-7}:rc-cli" # Remove the '.tar.gz' extension
-echo "done"
 
 # TODO: Run MLL Evaluation Script on `rc-out.json` along with the timings for `setup.sh` and `eval.sh`
-
-echo "Finished!"
-
 exec "$@"
