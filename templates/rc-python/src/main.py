48afb3dcc7d7911bf572338f51bbc32bab8d27a3
Luis Vasquez <luisvasq@mit.edu>
2021-02-06 13:07:50 +0100

    Fix debug command & update input data samples

--- templates/rc-python/src/main.py
+++ templates/rc-python/src/main.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     with open(path.join(OUTPUTS_DIR, '{}-out.json'.format(run_mode)), 'w') as out_file:
         dump({ run_mode: 'Hello World from the app!' }, out_file)
         print((
-            "2. A file '{0}-out.json' has been written"
-            " in the directory '{0}_outputs'\n".format(run_mode)
+            "2. A file '{0}-out.json' has been written in the directory"
+            " './app/data/{0}_outputs'".format(run_mode)
         ))
