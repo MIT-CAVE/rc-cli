77694ba0b8cd6050188f1b955d6e5805ba0b3155
Luis Vasquez <luisvasq@mit.edu>
2021-02-09 16:51:21 +0100

    Update rc-cli (a lot)

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Install the RC CLI on Linux
+
+# Constants
+readonly INSTALL_DIR="${HOME}"
+readonly GITHUB_ORG_NAME="MIT-CAVE"
+readonly GITHUB_REPO_NAME="amzn-docker-2021"
+
+#######################################
+# Display an error message when the user input is invalid.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+err() {
+  echo "$0: $1" >&2
+}
+
+main() {
+  # Check if a previous installation exists
+  if [ -d "${INSTALL_DIR}" ]; then
+    err "found an existing installation of RC CLI"
+    exit 1
+  else
+    mkdir -p "${INSTALL_DIR}"
+    git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPO_NAME}" \
+      --depth=1 \
+      ${INSTALL_DIR}
+  fi
+  #
+  # export RC_CLI_PATH="${HOME}/.rc_cli"
+  # [ -s "${RC_CLI_PATH}/rc-cli.sh" ] && command . "${RC_CLI_PATH}/rc-cli.sh"
+
+  printf "rc-cli has been installed successfully."
+}
+
+main "$@"
--- solutions/README.md
+++
@@ -1,7 +0,0 @@
-# What is this directory for?
-
-The current directory serves as a mount point for Docker to allow the participants' Docker images to be validated by the `rctrain` instance. The participants should be encouraged to save their Docker image solutions in this folder and validate them through the trainer, before submitting their saved images through the web platform.
-
-All files in this directory must be ignored by the Git repository, except for this README file (see [.gitignore](.gitignore)).
-
-The directory and its files will be mounted as a [read-only bind mount](https://docs.docker.com/storage/bind-mounts/#use-a-read-only-bind-mount), at the Docker run-time stage.
