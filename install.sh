687939fdc1cf7aac6e7cdb962214fb98fcc04ba2
Connor Makowski <connorman528@gmail.com>
2021-02-10 08:12:24 -0500

    First pass at install.sh

--- install.sh
+++ install.sh
@@ -3,40 +3,106 @@
 # Install the RC CLI on Linux
 
 # Constants
+readonly CHARS_LINE="============================"
 readonly INSTALL_DIR="${HOME}"
+readonly INSTALL_NAME=".rc-cli"
+readonly CLI_NAME="rc-cli"
 readonly GITHUB_ORG_NAME="MIT-CAVE"
-readonly GITHUB_REPO_NAME="amzn-docker-2021"
+readonly GITHUB_REPO_NAME="rc-cli"
+readonly MIN_DOCKER_VERSION="18.09.00"
 
-#######################################
-# Display an error message when the user input is invalid.
-# Globals:
-#   None
-# Arguments:
-#   None
-# Returns:
-#   None
-#######################################
-err() {
+err() { # Display an error message
   echo "$0: $1" >&2
 }
 
-main() {
-  # Check if a previous installation exists
-  if [ -d "${INSTALL_DIR}" ]; then
-    err "found an existing installation of RC CLI"
+check_os() { # Validate that the current OS
+  case "$(uname -s)" in
+      Linux*)     machine="Linux";;
+      Darwin*)    machine="Mac";;
+      *)          machine="UNKNOWN"
+  esac
+  if [ $machine = "UNKNOWN" ]; then
+    printf "Error: Unknown operating system.\n"
+    printf "Please run this command on one of the following:\n"
+    printf "- MacOS\n- Linux\n- Windows (With Ubuntu 20.04 on Windows Subsystem for Linux 2 - WSL2)"
     exit 1
-  else
-    mkdir -p "${INSTALL_DIR}"
-    git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPO_NAME}" \
-      --depth=1 \
-      ${INSTALL_DIR}
-    ln -sf ${INSTALL_DIR}/${GITHUB_REPO_NAME}/rc-cli.sh ${HOME}/.local/bin/rc-cli
   fi
-  #
-  # export RC_CLI_PATH="${HOME}/.rc_cli"
-  # [ -s "${RC_CLI_PATH}/rc-cli.sh" ] && command . "${RC_CLI_PATH}/rc-cli.sh"
+}
 
-  # printf "rc-cli has been installed successfully."
+check_docker() { # Validate docker is installed
+  if [ ! $(docker --version) ]; then
+    err "Docker not installed. Please install version ${MIN_DOCKER_VERSION} or greater"
+    exit 1
+  fi
+  CURRENT_DOCKER_VERSION=$(docker --version | sed -e 's/Docker version \(.*\), build.*/\1/')
+  if [ $(\
+    "$(printf '%s\n' "$MIN_DOCKER_VERSION" "$CURRENT_DOCKER_VERSION" \
+    | sort -V \
+    | head -n1)" = "$MIN_DOCKER_VERSION") ]
+  then
+    err "Your current Docker version ($CURRENT_DOCKER_VERSION) is too old. Please install Docker version (${MIN_DOCKER_VERSION}) or greater"
+    exit 1
+  fi
+}
+
+check_previous_installation() { # Check to make sure previous installations are removed before continuing
+  if [ -d "${INSTALL_DIR}/${INSTALL_NAME}" ]; then
+    printf "An existing installation of RC CLI was found \nLocation: ${INSTALL_DIR}/${INSTALL_NAME}\n"
+    read -r -p "Would you like to overwrite this installation? [y/N] " input
+    case ${input} in
+      [yY][eE][sS] | [yY])
+        printf "Removing old installation... "
+        rm -rf "${INSTALL_DIR}/${INSTALL_NAME}"
+        printf "done\n"
+        ;;
+      [nN][oO] | [nN] | "")
+        err "Installation canceled"
+        exit 1
+        ;;
+      *)
+        err "invalid input: Installation canceled"
+        exit 1
+        ;;
+    esac
+  fi
+}
+
+install_new() { # Copy the needed files locally
+  printf "Creating application folder at '${INSTALL_DIR}/${INSTALL_NAME}'..."
+  mkdir -p "${INSTALL_DIR}/${INSTALL_NAME}"
+  printf "done\n"
+  printf "${CHARS_LINE}\n"
+  printf "Cloning in application contents from 'https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPO_NAME}':\n"
+  git clone "git@github.com:${GITHUB_ORG_NAME}/${GITHUB_REPO_NAME}" \
+    --depth=1 \
+    "${INSTALL_DIR}/${INSTALL_NAME}"
+  printf "${CHARS_LINE}\n"
+  if [ ! -d "${INSTALL_DIR}/${INSTALL_NAME}" ]; then
+    err "Git Clone Failed. Installation Canceled"
+    exit 1
+  fi
+}
+
+add_to_path() { # Add the cli to a globally accessable path
+  printf "Making '${CLI_NAME}' globally accessable: \nCreating link from '${INSTALL_DIR}/${INSTALL_NAME}/${CLI_NAME}.sh' as '/usr/src/bin/${CLI_NAME}'..."
+  sudo ln -sf "${INSTALL_DIR}/${INSTALL_NAME}/${CLI_NAME}.sh" "/usr/local/bin/${CLI_NAME}"
+  printf "done\n"
+}
+
+success_message() { # Send a success message to the user on successful installation
+  printf "${GITHUB_REPO_NAME} has been successfully installed \n"
+  printf "You can verify the installation with 'rc-cli --version'\n"
+  printf "To get started use 'rc-cli --help'\n"
+}
+
+main() {
+  check_os
+  check_docker
+  # TODO check_git
+  check_previous_installation
+  install_new
+  add_to_path
+  success_message
 }
 
 main "$@"
