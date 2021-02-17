3854adef98c5f666066868c5caee93874ec0b94b
Luis Vasquez <luisvasq@mit.edu>
2021-02-17 22:51:27 +0100

    Update exception handlers & console output format

--- templates/rc-python/src/evaluate.py
+++ templates/rc-python/src/evaluate.py
@@ -10,44 +10,71 @@ SETUP_OUTPUTS_DIR = path.join(DATA_DIR, 'setup_outputs')
 EVALUATE_INPUTS_DIR = path.join(DATA_DIR, 'evaluate_inputs')
 EVALUATE_OUTPUTS_DIR = path.join(DATA_DIR, 'evaluate_outputs')
 
+# I/O Files
+SETUP_OUTPUT_FILEPATH = path.join(SETUP_OUTPUTS_DIR, 'setup-out.json')
+EVALUATE_INPUT_FILENAME = path.join(EVALUATE_INPUTS_DIR, 'evaluate-in.csv')
+EVALUATE_OUTPUT_FILENAME = path.join(EVALUATE_OUTPUTS_DIR, 'predicted_routes.json')
+
 # Read input data
 print('Reading Input Data')
-with open(path.join(EVALUATE_INPUTS_DIR, 'evaluate-in.csv'), newline='') as evaluate_in:
-    evaluate_in = [i for i in csv.reader(evaluate_in, delimiter=',', quotechar='|')]
+# Setup output
+setup_out = None
+try:
+    with open(SETUP_OUTPUT_FILEPATH) as in_file:
+        setup_out = json.load(in_file)
+except FileNotFoundError:
+    print("The '{}' file is missing!".format(SETUP_OUTPUT_FILEPATH))
+except JSONDecodeError:
+    print("Error in the '{}' JSON data!".format(SETUP_OUTPUT_FILEPATH))
+except Exception as e:
+    print("Error when reading the '{}' file!".format(SETUP_OUTPUT_FILEPATH))
+    print(e)
+# Evaluate input
+evaluate_in = None
+try:
+    with open(EVALUATE_INPUT_FILENAME, newline='') as in_file:
+        evaluate_in = [
+            i for i in csv.reader(in_file, delimiter=',', quotechar='|')
+        ]
+except FileNotFoundError:
+    print("The '{}' file is missing!".format(EVALUATE_INPUT_FILENAME))
+except csv.Error:
+    print("Error in the '{}' CSV data!".format(EVALUATE_INPUT_FILENAME))
+except Exception as e:
+    print("Error when reading the '{}' file!".format(EVALUATE_INPUT_FILENAME))
+    print(e)
 
-with open(path.join(SETUP_OUTPUTS_DIR, 'setup-out.json')) as setup_out:
-    try:
-        setup_out = json.load(setup_out)
-    except:
-        print('setup_out.json is missing!')
-        setup_out=None
+if setup_out and evaluate_in:
+    print('Printing Input Data')
+    print('model from setup_out:', setup_out)
+    print('data for evaluate_in:', evaluate_in)
+    time.sleep(1)
+    # Solve for life the universe and everything
+    print('{} Algorithm'.format(setup_out.get("Algorithm","No Provided")))
+    time.sleep(1)
+    print('Solving Dark Matter Waveforms')
+    time.sleep(1)
+    print('Quantum Computer is Overheating')
+    time.sleep(1)
+    print('Trying Alternate Measurement Cycles')
+    time.sleep(1)
+    print('Found a Great Solution!')
+    time.sleep(1)
+    print('Checking Validity')
+    time.sleep(1)
+    print('The Answer is 42!')
+    output={
+        'Message':'Hello from the evaluation script!',
+        'The_Answer_To_Life':42
+    }
 
-print('Printing Input Data')
-print('model from setup_out:', setup_out)
-print('data for evaluate_in:', evaluate_in)
-time.sleep(1)
-# Solve for life the universe and everything
-print('{} Algorithm'.format(setup_out.get("Algorithm","No Provided")))
-time.sleep(1)
-print('Solving Dark Matter Waveforms')
-time.sleep(1)
-print('Quantum Computer is Overheating')
-time.sleep(1)
-print('Trying Alternate Measurement Cycles')
-time.sleep(1)
-print('Found a Great Solution!')
-time.sleep(1)
-print('Checking Validity')
-time.sleep(1)
-print('The Answer is 42!')
-output={
-    'Message':'Hello from the evaluation script!',
-    'The_Answer_To_Life':42
-}
-
-# Write output data
-with open(path.join(EVALUATE_OUTPUTS_DIR, 'predicted_routes.json'), 'w') as out_file:
-    json.dump(output, out_file)
-    print((
-        "Evaluate: A file, 'predicted_routes.json', has been saved in {}".format(EVALUATE_OUTPUTS_DIR)
-    ))
+    # Write output data
+    with open(path.join(
+        EVALUATE_OUTPUTS_DIR,
+        EVALUATE_OUTPUT_FILENAME
+    ), 'w') as out_file:
+        json.dump(output, out_file)
+        print("Evaluate: A file, {0}, has been saved in {1}".format(
+            EVALUATE_OUTPUT_FILENAME,
+            EVALUATE_OUTPUTS_DIR
+        ))
