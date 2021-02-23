d6961baa3f481423c7d582d9d376fd540ddd603d
mrkwanzaa <sledgeguter@gmail.com>
2021-02-23 17:00:53 -0500

    Add rc-bash template

---
+++ templates/rc-bash/src/setup.sh
@@ -0,0 +1,19 @@
+echo "Initializing Quark Reducer"
+sleep 1
+echo "Placing Nano Tubes In Gravitational Wavepool"
+sleep 1
+echo "Measuring Particle Deviations"
+sleep 1
+echo "Programming Artificial Noggins"
+sleep 1
+echo "Beaming in Complex Materials"
+sleep 1
+echo "Solving Model"
+sleep 1
+echo "Saving Solved Model State"
+sleep 1
+echo "{
+      'Message':'Hello from the setup.py script!',
+      'Algorithm':\"Initialize Gizmo\",
+      'Algorithm_Parameter': 32
+  }" > data/setup_outputs/setup-out.json
