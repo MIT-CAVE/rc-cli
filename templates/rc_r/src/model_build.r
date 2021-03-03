46796fdff317af73179ad5f98463276764ad51a2
Tim Russell <47668639+mitrussell@users.noreply.github.com>
2021-03-03 11:48:03 -0500

    Update model_build.r

--- templates/rc_r/src/model_build.r
+++ templates/rc_r/src/model_build.r
@@ -19,4 +19,4 @@ Sys.sleep(1)
 # write an output file
 cat("{\n    'Model':'Hello from the model_build.r script!',    \n    'sort_by':'lat'\n}", file="data/model_build_outputs/model.json")
 
-print(paste("Success: The '",getwd(),"/data/model_build_outputs/model.json' file has been saved.",sep = ""))
+print(paste("Success: The '", getwd(),"/data/model_build_outputs/model.json' file has been saved.", sep = ""))
